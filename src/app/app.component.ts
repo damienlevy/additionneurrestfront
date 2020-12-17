754dbde3ae262da1efefea00b1092379adec9451
Damien LEVY <damien.levy4@etu.univ-lorraine.fr>
2020-12-17 22:48:06 +0100

    frontend additionneur rest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+
+export class AppComponent implements  OnInit{
   title = 'projetSpringBootEnvironnementFront';
+  current = '0';
+  num: number;
+
+  ngOnInit(): void {
+    this.httpClient.get('http://localhost:8080/adder/current', {responseType: 'text'}).subscribe(data => {
+      this.current = data;
+      console.log(this.current);
+    });
+  }
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  random(): void{
+      console.log('click');
+      this.httpClient.get('http://localhost:8080/adder/random', {responseType: 'text'}).subscribe(data => {
+      this.current = data;
+    });
+  }
+  addition(): void {
+    console.log(this.num);
+    this.httpClient.post('http://localhost:8080/adder/accumulate', this.num).subscribe(
+      () => { this.ngOnInit();
+      }
+      );
+
+
+  }
+
+  changeCurrent(): void {
+    this.httpClient.post('http://localhost:8080/adder/changeBase', this.current).subscribe(() => {
+      this.ngOnInit();
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+
+
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
